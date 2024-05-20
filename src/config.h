24377815a0a5d174aba13870b1f2a1af6b44d1fa
Tim <sakunamary@gmail.com>
2024-05-20 15:13:14 +0800

    update

--- src/config.h
+++ src/config.h
@@ -16,7 +16,7 @@
 ///////////////////////////////////////
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
 #define BAUDRATE 115200        // serial port baudrate
-#define BAUD_HMI 9600
+#define BAUD_HMI 57600
 // pinout define
 #define ENC_BUTTON 7
 #define ENC_CLK 15
